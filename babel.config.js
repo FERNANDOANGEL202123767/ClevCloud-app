518898060c962f4eb440bcac6414486b307fc99c
FERNANDOANGEL202123767 <fernandoangelhernandez62@gmail.com>
2025-10-09 21:15:59 -0600

    Fix settings.gradle duplicado y configura Expo Modules

--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,37 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'module:react-native-dotenv',
-      {
-        moduleName: '@env',
-        path: '.env',
-        safe: false,
-        allowUndefined: true,
-      },
-    ],
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
-        alias: {
-          '@': './src',
-          '@components': './src/components',
-          '@screens': './src/screens',
-          '@api': './src/api',
-          '@hooks': './src/hooks',
-          '@utils': './src/utils',
-          '@types': './src/types',
-          '@config': './src/config',
-          '@store': './src/store',
-          '@styles': './src/styles',
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'], // Usa preset de Expo (incluye RN preset)
+    plugins: [
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          safe: false,
+          allowUndefined: true,
         },
-      },
+      ],
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@api': './src/api',
+            '@hooks': './src/hooks',
+            '@utils': './src/utils',
+            '@types': './src/types',
+            '@config': './src/config',
+            '@store': './src/store',
+            '@styles': './src/styles',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin', // Debe estar al final
     ],
-    'react-native-reanimated/plugin',
-  ],
+  };
 };
